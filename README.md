5083223a23d45b5fd747ba4d9494490db4fd6afa
ethancschwab <schwabe543@gmail.com>
2020-04-04 21:01:33 -0400

    Cleanup, Media

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 Elite spotify stats!
 
-
+Work In Progress Site (login to display your top tracks): http://elitespotifystats.s3-website-us-west-1.amazonaws.com/
 
 ![Architecture](https://github.com/ethancschwab/elite-spotify-stats/blob/master/EliteStats.jpg)
--- frontend/curl.js
+++ frontend/curl.js
@@ -3,26 +3,20 @@ const api_url = 'https://1z3pmsgw4f.execute-api.us-west-1.amazonaws.com/default/
 Http.open("POST", api_url,true);
 
 
-console.log(Http);
-console.log(window.location.href);
+// Fetch Access Token from URL
 var current_url = window.location.href;
 var token = current_url.split("=")[1].split("&")
-console.log(token)
-
 var token_one="token="
 var token_final=token_one.concat(token[0])
-console.log(token_final)
 
+// Send Request with Access Token
 Http.send(token_final);
-
 
 
 Http.onreadystatechange=(e)=>{
 	console.log(Http.responseText)
 	var output = document.getElementById('output');
-	console.log(typeof Http.responseText);
   	output.innerHTML = Http.responseText;
-//	document.body.innerHTML = Http.responseText
 }
 
 Http.onerror=(e)=>{
